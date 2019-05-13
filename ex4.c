c04d52cfc914390f7673d3bc7234728d6440f64d
spikeburton <ralphburton3@gmail.com>
2019-05-12 23:20:17 -0400

    add ex4

---
+++ ex4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void crash() {
+  char *test = NULL;
+
+  for (int i = 0; i < 1000000000; i++) {
+    printf("%c", test[i]);
+  }
+}
+
+int main()
+{
+  int age = 10;
+  int height = 72;
+
+  printf("I am %d years old.\n", age);
+  // printf("I am %d inches tall.\n", height);
+  crash();
+
+  return 0;
+}
