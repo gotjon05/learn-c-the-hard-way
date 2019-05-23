e0db05426c4e16bca3e8726e5f222158859a7047
spikeburton <ralphburton3@gmail.com>
2019-05-23 02:25:55 -0400

    check for existence of test before running

--- c-skeleton/tests/runtests.sh
+++ c-skeleton/tests/runtests.sh
@@ -2,13 +2,16 @@ echo "Running unit tests:"
 
 for i in tests/*_tests
 do
-  $i 2>&1 >> tests/tests.log
-  if [ ! $? ]
+  if test -f $i
   then
-    echo "ERROR in test $i: here's tests/tests.log"
-    echo "------"
-    tail tests/tests/log
-    exit 1
+    $i 2>&1 >> tests/tests.log
+    if [ ! $? ]
+    then
+      echo "ERROR in test $i: here's tests/tests.log"
+      echo "------"
+      tail tests/tests/log
+      exit 1
+    fi
   fi
 done
 
